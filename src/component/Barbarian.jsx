066461933f7a45bf1bcff272108d4740f2dfd4d4
davekishan <75985572+davekishan@users.noreply.github.com>
2023-11-02 11:54:39 +0530

    new changes

---
+++ src/component/Barbarian.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Barbarian = () => {
+  return (
+    <div>
+      <div class="wrapper">
+        <div class="clash-card barbarian">
+          <div class="clash-card__image clash-card__image--barbarian">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/barbarian.png"
+              alt="barbarian"
+            />
+          </div>
+         
+          <div class="clash-card__unit-name">The Barbarian</div>
+          <div class="clash-card__unit-description">
+            The Barbarian is a kilt-clad Scottish warrior with an angry,
+            battle-ready expression, hungry for destruction. He has Killer
+            yellow horseshoe mustache.
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
