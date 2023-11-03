3ca64b54732fe7606e4b1c418b2cf58b2a0b2dba
davekishan <75985572+davekishan@users.noreply.github.com>
2023-11-03 13:45:52 +0530

    navigate & contract change

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Battle } from './component/Battle';
-import { Home } from './component/Home';
-
+import { Battle } from "./component/Battle";
+import { Home } from "./component/Home";
 
 function App() {
-
   return (
     <>
-     <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              // eslint-disable-next-line react/jsx-pascal-case
-              <Home/>
-            }
-          />
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                // eslint-disable-next-line react/jsx-pascal-case
+                <Home />
+              }
+            />
 
-       
-        
-        </Routes>
-      </BrowserRouter>
+            <Route
+              exact
+              path="/battle"
+              element={
+                // eslint-disable-next-line react/jsx-pascal-case
+                <Battle />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </>
     </>
-     
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
