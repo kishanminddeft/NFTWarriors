614be9707c38c3281e74947b8e466098dbf8b7a5
davekishan <75985572+davekishan@users.noreply.github.com>
2023-11-07 19:33:15 +0530

    create attack and defence function

--- src/component/Battle.jsx
+++ src/component/Battle.jsx
@@ -4,8 +4,9 @@ import { Archer } from "./Archer";
 import { Giant } from "./Giant";
 import { Wizard } from "./Wizard";
 import { Goblin } from "./Goblin";
+import contract from "../utils/contract";
 
-export const Battle = () => {
+export const Battle = ({account,PlayerName,battle}) => {
 
   const [isAnimating, setIsAnimating] = useState(false);
 
@@ -17,8 +18,38 @@ export const Battle = () => {
     }, 2000);
   };
 
+  const attack=async()=>{
+    
+
+    const gasLimit = 300000; 
+    const result = await contract?.methods.attackOrDefendChoice('1',battle).send({ from:account,gas:gasLimit }).then((result) => {
+        // Handle the result of the method call
+        console.log('Method result:', result);
+    })
+        .catch((error) => {
+            // Handle errors
+            console.error('Error calling contract method:', error);
+        });
+
+  }
+
+  const defence=async()=>{
+    const gasLimit = 300000; 
+        
+    const result = await contract?.methods.attackOrDefendChoice(2,battle).send({ from: account,gas:gasLimit }).then((result) => {
+        // Handle the result of the method call
+        console.log('Method result:', result);
+    })
+        .catch((error) => {
+            // Handle errors
+            console.error('Error calling contract method:', error);
+        });
+    
+  }
+
   return (
     <div className="bg-image ">
+     
       <div
         className="card"
         style={{
@@ -33,6 +64,8 @@ export const Battle = () => {
       >
         <Barbarian />
       </div>
+      {battle ? <h2 style={{color:"white"}}>{battle}</h2> :<h2 style={{color:"white"}}>Battle Name</h2>}
+      
       <div
         className="card"
         style={{
@@ -45,29 +78,29 @@ export const Battle = () => {
           left: 0,
           
         }}
-      > <Goblin/></div>
+      > <Goblin/>{PlayerName ? <h3 style={{color:'white'}}>{PlayerName}</h3>:<h3 style={{color:'white'}}> PlayerName</h3>}</div>
       <div className="button-row">
-        <button class="btn" type="button">
+        <button className="btn" type="button" onClick={attack}>
           <strong>Attack </strong>
           <div id="container-stars">
             <div id="stars"></div>
           </div>
 
           <div id="glow">
-            <div class="circle"></div>
-            <div class="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
           </div>
         </button>
 
-        <button class="btn" type="button">
+        <button className="btn" type="button" onClick={defence}>
           <strong>Defence</strong>
           <div id="container-stars">
             <div id="stars"></div>
           </div>
 
           <div id="glow">
-            <div class="circle"></div>
-            <div class="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
           </div>
         </button>
       </div>
